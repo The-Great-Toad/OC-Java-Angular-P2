fca8dabdfa3916601389e1e3779781754dd0c4c2
The-Great-Toad <bearcode26@gmail.com>
2025-06-19 11:28:04 +0200

    Tech - Added ngx-charts dependency for visualizing Olympic data

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { map, Observable, of, Subscription } from 'rxjs';
+import { IOlympicCountry } from 'src/app/core/models/IOlympicCountry';
+import { IParticipation } from 'src/app/core/models/IParticipation';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
 @Component({
@@ -12,7 +14,47 @@ export class HomeComponent implements OnInit {
   private olympicService = inject(OlympicService);
   public olympics$: Observable<any> = of(null);
 
+  // Chart options
+  public view: [number, number] = [700, 400];
+  public results$!: Subscription;
+  public results: any[] = [];
+  public gradient: boolean = true;
+  public showLegend: boolean = false;
+  public showLabels: boolean = true;
+  public isDoughnut: boolean = false;
+  public legendPosition: string = 'below';
+
+  public colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA'],
+  };
+
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
+    this.results$ = this.olympics$.subscribe({
+      next: (olympicCountries: IOlympicCountry[]) => {
+        this.results = olympicCountries.map(
+          (olympicCountry: IOlympicCountry) => ({
+            name: olympicCountry.country,
+            value: (olympicCountry.participations as IParticipation[])
+              .map((participation: IParticipation) => participation.medalsCount)
+              .reduce((acc, curr) => acc + curr, 0),
+          })
+        );
+        console.log('Results:', this.results);
+      },
+      error: (err) => console.error('Error fetching olympics data', err),
+    });
+  }
+
+  public onSelect(data: any): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  public onActivate(data: any): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  public onDeactivate(data: any): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
   }
 }
